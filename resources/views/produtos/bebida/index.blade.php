e874f66ef1d2a1b9e30891b1c13563822c49bf15
Lucas <lucasmacedosantos1998@gmail.com>
2019-05-13 02:26:44 -0300

    modificacoes

---
+++ resources/views/produtos/bebida/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.tema')
+@section('titulo', 'Bebidas Index')
+
+@section('conteudo')
+
+  <div class="row">
+    <div class="card">
+    <div class="card-header"> Bebidas Index </div>
+      <div class="card-body">
+
+        <table id="table">
+          <thead>
+            <th></th>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+
+@endsection
---
+++ resources/views/produtos/porcao/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.tema')
+@section('titulo', 'Porção Index')
+
+@section('conteudo')
+
+  <div class="row">
+    <div class="card">
+    <div class="card-header"> Porção Index </div>
+      <div class="card-body">
+
+        <table id="table">
+          <thead>
+            <th></th>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+
+@endsection
